c237f1280dd6686e90988a974cd63cc07c28d8b5
ajayreddypalnati <ajayreddypalnati@gmail.com>
2026-06-21 21:13:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ import io
 # Silence terminal spam
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
-st.set_page_config(page_title="9-EMA Swing Screener", page_icon="⚡", layout="wide", initial_sidebar_state="collapsed")
+st.set_page_config(page_title="9-EMA Momentum Screener", page_icon="⚡", layout="wide", initial_sidebar_state="collapsed")
 
 # ==========================================
 # 1. CSS INJECTION (Premium Navy & Cream Theme)
@@ -437,7 +437,7 @@ current_date = datetime.now(ist).strftime('%d %b %Y')
 st.markdown(f"""
     <div class="premium-header">
         <div class="header-left">
-            <div class="header-title">⚡ 9-EMA Swing Screener</div>
+            <div class="header-title">⚡ 9-EMA momentum Swing Screener</div>
             <div class="header-subtitle">Refreshed every 1 minute paired with Sector, Industry & Momentum rank.</div>
         </div>
         <div class="header-right">
